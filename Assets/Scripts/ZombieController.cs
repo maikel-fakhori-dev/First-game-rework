d9b879be2a81e66213c4588aeb1ae0b5ec7c1641
maikel-fakhori-dev <gamedevmaikel@gmail.com>
2026-08-08 22:19:19 +0100

    Got animations for zombies
    We got animations for the zombies but must fix something in damage as the player keeps taking damage although we use oncollisionenter

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -7,17 +7,20 @@ public class ZombieController : MonoBehaviour
     public float zombieSpeed = 1f;
     public float zombieDamage = 10f;
     public float zombieHealth = 100f;
-    
     public int scoreAdd;
-    
+    public Animator zombieAnimator;
 
+    private bool canAttack;
     private GameObject player;
     private ZombieSpawnManager spawnManager;
+    
     //private Rigidbody zombieRb;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+        zombieAnimator = GetComponentInChildren<Animator>();
         //zombieRb = GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
         spawnManager = GameObject.Find("Zombie Spawn Manager").GetComponent<ZombieSpawnManager>();
@@ -26,14 +29,20 @@ public class ZombieController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 playerDirection = (player.transform.position - transform.position).normalized;
+        canAttack = zombieAnimator.GetBool("canAttack");
+        Vector3 playerDistance = player.transform.position - transform.position;
+        Vector3 playerDirection = playerDistance.normalized;
         //zombieRb.AddForce(playerDirection * zombieSpeed);
         //transform.Translate(playerDirection * zombieSpeed);
 
         playerDirection.y = 0;
 
         transform.rotation = Quaternion.LookRotation(playerDirection);
-        transform.Translate(new Vector3(0,0,zombieSpeed));
+
+        if (canAttack == false)
+        {
+            transform.Translate(new Vector3(0, 0, zombieSpeed));
+        }
        
     }
     void Update()
@@ -48,12 +57,19 @@ public class ZombieController : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject == player)
         {
             player.GetComponent<PlayerController>().playerHealth -= zombieDamage;
+            zombieAnimator.SetBool("canAttack",true);
         }
-
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject == player)
+        {
+            zombieAnimator.SetBool("canAttack", false);
+        }
     }
 }
