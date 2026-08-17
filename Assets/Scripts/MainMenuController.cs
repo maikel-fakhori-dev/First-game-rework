901ee93edd6d25ecb6d5b414eb1c19abcef4708e
maikel-fakhori-dev <gamedevmaikel@gmail.com>
2026-08-17 18:36:59 +0100

    working main menu that starts game and quits game

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenuController : MonoBehaviour
+{
+    public void OnStartClick()
+    {
+        SceneManager.LoadScene("Zombie Fight Scene");
+    }
+
+    public void OnExitClick()
+    {
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
