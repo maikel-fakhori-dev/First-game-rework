bb1127c5b7387e08db31ca3812f1feed384266a2
maikel-fakhori-dev <gamedevmaikel@gmail.com>
2026-08-20 17:24:25 +0100

    fixed all bugs for main menu

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,10 +2,19 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MainMenuController : MonoBehaviour
 {
-    
+    private GameObject player;
+
+    private PlayerController playerController;
+    private Weapon weapon;
+
     public void OnStartClick()
     {
         SceneManager.LoadScene("Zombie Fight Scene");
+        player = GameObject.Find("Player");
+        playerController = player.GetComponent<PlayerController>();
+        weapon = playerController.GetComponentInChildren<Weapon>();
+        playerController.enabled = true;
+        weapon.enabled = true;
     }
 
     public void OnExitClick()
