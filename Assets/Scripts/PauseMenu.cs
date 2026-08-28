c6948c3e11863475a5107d638494a7c2f82068da
maikel-fakhori-dev <gamedevmaikel@gmail.com>
2026-08-28 15:19:09 +0100

    a few setting changes

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -21,7 +21,7 @@ public class PauseMenu : MonoBehaviour
     }
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.P))
         {
             if (isPaused)
             {
